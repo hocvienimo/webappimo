76f1ddcda83059d680e0b2c4558e638cfaf39b0d
dmotthung <dmotthung@gmail.com>
2024-08-23 15:29:26 +0700

    complet danh mục sản phẩm at 15:30 - 23/08/2024

--- app/kien-thuc/[category]/CategoryPageWrapper.tsx
+++ app/kien-thuc/[category]/CategoryPageWrapper.tsx
@@ -1,50 +1,106 @@
-// app/kien-thuc/[category]/CategoryPageWrapper.tsx
 "use client"
 
-import { useEffect, useState } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
-import NavHeader from '@/app/components/NavHeader';
-import { allArticles } from '../../data/ArticlesData';
-import { notFound } from 'next/navigation';
+import { useEffect, useState } from "react";
+import { notFound, usePathname, useSearchParams } from 'next/navigation';
+import { allArticles } from "@/app/data/ArticlesData";
+import NavHeader from "@/app/components/NavHeader";
+import Link from "next/link";
+import Image from "next/image";
+import BannerSb from '@/public/images/dich-vu-marketing-online-sb.jpg'
+
 
 const CategoryPageWrapper = ({ children }) => {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const category = decodeURIComponent(pathname.split('/').pop() || '');
-  const [isNotFound, setIsNotFound] = useState(false);
 
-  useEffect(() => {
-    // Kiểm tra danh mục
-    const articles = allArticles.filter(article => article.categoryLink === pathname);
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+  
+    const category = decodeURIComponent(pathname.split('/').pop() || '');
+    const [categories, setCategories] = useState([]);
+    const [currentCategoryName, setCurrentCategoryName] = useState('');
+    const [filteredArticles, setFilteredArticles] = useState([]);
 
-    if (articles.length === 0) {
-      setIsNotFound(true);
-      notFound(); // Chuyển hướng đến trang 404 nếu danh mục không tồn tại
-    }
-  }, [pathname]);
+    useEffect(() => {
+        // Lọc bài viết theo categoryLink
+        const articles = allArticles.filter(article => article.categoryLink === pathname);
+        if (articles.length === 0) {
+          notFound(); // Chuyển hướng đến trang 404 nếu danh mục không tồn tại
+          return; // Ngăn không chạy các đoạn mã còn lại
+        }
+        setFilteredArticles(articles);
+    
+        // Lấy danh sách danh mục từ allArticles
+        const uniqueCategories = Array.from(new Set(allArticles.map(article => article.categoryLink)))
+        .map(link => {
+            const category = allArticles.find(article => article.categoryLink === link);
+            return {
+            name: category?.category || 'Unknown',
+            link
+            };
+        });
+        setCategories(uniqueCategories);
+    
+        // Tìm tên danh mục hiện tại
+        const currentCategory = uniqueCategories.find(category => category.link === pathname);
+        setCurrentCategoryName(currentCategory?.name || 'Danh mục không xác định');
+    
+      }, [pathname]);
 
-  if (isNotFound) {
-    return null; // Không hiển thị nội dung khi danh mục không tồn tại
-  }
-
-  const heading = {
+    const heading = currentCategoryName ? {
     h1: {
-      title: 'Danh Mục'
+        title: currentCategoryName
     }
-  };
+    }: null;
 
-  const crumbs = [
+    const crumbs = [
     { href: '/', label: 'Trang chủ' },
     { href: '/kien-thuc', label: 'Kiến thức' },
-    { href: pathname, label: 'Danh Mục' }
-  ];
+    { href: pathname, label: currentCategoryName }
+    ];
 
   return (
     <>
-      <NavHeader heading={heading} crumbs={crumbs} />
-      {children}
-    </>
-  );
-};
+    {heading && (
+        <div className="mt-20">
+        <NavHeader crumbs={crumbs} heading={heading} />
+        </div>
+    )}
+    <main className="container mx-auto px-4 py-8">
+        <div className="grid md:grid-cols-4 grid-cols-1 md:gap-6">
+            <div className="col-span-3">
+            {children}
+            </div>
+            {/* Sidebar bên phải - Sticky */}
+            <aside className="sticky top-28 self-start mt-6 md:mt-0">
+                <div className="bg-gray-100 p-4 rounded-lg">
+                <h3 className="text-2xl font-secondary font-semibold text-secondary mb-4">Chuyên Đề</h3>
+                <ul>
+                    {categories.map((category) => (
+                    <li key={category.name}>
+                        <Link href={category.link} className={`block py-1 text-black ${category.link === pathname ? 'text-thirdary' : 'hover:text-thirdary'}`}>
+                        {category.name}
+                        </Link>
+                    </li>
+                    ))}
+                </ul>
+                </div>
 
-export default CategoryPageWrapper;
+                <div className="banner-sb mt-4">
+                <Link href="/dich-vu-marketing-online" passHref className='block h-full'>
+                <div className="w-full h-full relative">
+                    <Image
+                    src={BannerSb}
+                    alt= 'dich-vu-marketing-online-sb'
+                    style={{ objectFit: 'cover' }}
+                    className="rounded-lg"
+                    />
+                </div>
+                </Link>
+            </div>
+            </aside>
+        </div>
+    </main>
+    </>
+  )
+}
+
+export default CategoryPageWrapper
--- app/kien-thuc/[category]/page.tsx
+++ app/kien-thuc/[category]/page.tsx
@@ -6,45 +6,22 @@ import Link from 'next/link'
 import { allArticles } from '../../data/ArticlesData'
 import Image from 'next/image'
 import PaginationComponent from '../../components/PaginationComponent'
-import BannerSb from '@/public/images/dich-vu-marketing-online-sb.jpg'
-import NavHeader from '@/app/components/NavHeader';
+import CategoryPageWrapper from './CategoryPageWrapper'
 
 const CategoryPage = () => {
-  const itemsPerPage = 9;
+  const itemsPerPage = 6;
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  const category = decodeURIComponent(pathname.split('/').pop() || '');
-
   const [currentPage, setCurrentPage] = useState(1);
   const [filteredArticles, setFilteredArticles] = useState([]);
-  const [categories, setCategories] = useState([]);
   const [currentCategoryName, setCurrentCategoryName] = useState('');
 
   useEffect(() => {
-    // Lọc bài viết theo categoryLink
     const articles = allArticles.filter(article => article.categoryLink === pathname);
-    if (articles.length === 0) {
-      notFound(); // Chuyển hướng đến trang 404 nếu danh mục không tồn tại
-      return; // Ngăn không chạy các đoạn mã còn lại
-    }
     setFilteredArticles(articles);
-
-    // Lấy danh sách danh mục từ allArticles
-    const uniqueCategories = Array.from(new Set(allArticles.map(article => article.categoryLink)))
-    .map(link => {
-        const category = allArticles.find(article => article.categoryLink === link);
-        return {
-        name: category?.category || 'Unknown',
-        link
-        };
-    });
-    setCategories(uniqueCategories);
-
-    // Tìm tên danh mục hiện tại
-    const currentCategory = uniqueCategories.find(category => category.link === pathname);
-    setCurrentCategoryName(currentCategory?.name || 'Danh mục không xác định');
-
+    
+    setCurrentCategoryName(articles[0]?.category || ''); // Set the category name
   }, [pathname]);
 
   useEffect(() => {
@@ -52,45 +29,22 @@ const CategoryPage = () => {
     setCurrentPage(page);
   }, [searchParams]);
 
-    // Tính tổng số trang
   const totalPages = Math.ceil(filteredArticles.length / itemsPerPage);
-  // Lấy bài viết dựa trên trang hiện tại
   const paginatedArticles = filteredArticles.slice(
     (currentPage - 1) * itemsPerPage,
     currentPage * itemsPerPage
   );
 
-    // Tạo liên kết phân trang
   const buildPageLink = (pageNumber) => {
     return `${pathname}?page=${pageNumber}`;
   };
 
-  const heading = currentCategoryName ? {
-    h1: {
-      title: currentCategoryName
-    }
-  }: null;
-
-  const crumbs = [
-    { href: '/', label: 'Trang chủ' },
-    { href: '/kien-thuc', label: 'Kiến thức' },
-    { href: pathname, label: currentCategoryName }
-  ];
-
 
   return (
     
-    <>
-    {heading && (
-      <div className="mt-20">
-        <NavHeader crumbs={crumbs} heading={heading} />
-      </div>
-    )}
-
-    <main className="container mx-auto px-4 py-8">
-        <div className="grid md:grid-cols-4 grid-cols-1 md:gap-6">
+    <CategoryPageWrapper>
         {/* Cột chính chứa các bài viết */}
-        <div className="col-span-3">
+
         {paginatedArticles.length > 0 ? (
             <div className="grid md:grid-cols-3 gap-6">
             {paginatedArticles.map((article) => (
@@ -138,40 +92,8 @@ const CategoryPage = () => {
                 totalPages={totalPages} 
                 buildPageLink={buildPageLink} 
                 />
-            )}
-        </div>
-
-        {/* Sidebar bên phải - Sticky */}
-        <aside className="sticky top-28 self-start mt-6 md:mt-0">
-            <div className="bg-gray-100 p-4 rounded-lg">
-              <h3 className="text-2xl font-secondary font-semibold text-secondary mb-4">Chuyên Đề</h3>
-              <ul>
-                  {categories.map((category) => (
-                  <li key={category.name}>
-                      <Link href={category.link} className={`block py-1 text-black ${category.link === pathname ? 'text-thirdary' : 'hover:text-thirdary'}`}>
-                      {category.name}
-                      </Link>
-                  </li>
-                  ))}
-              </ul>
-            </div>
-
-            <div className="banner-sb mt-4">
-            <Link href="/dich-vu-marketing-online" passHref className='block h-full'>
-              <div className="w-full h-full relative">
-                <Image
-                  src={BannerSb}
-                  alt= 'dich-vu-marketing-online-sb'
-                  style={{ objectFit: 'cover' }}
-                  className="rounded-lg"
-                />
-              </div>
-            </Link>
-          </div>
-        </aside>
-        </div>
-    </main>
-    </>
+            )}    
+    </CategoryPageWrapper>
   );
 };
 
